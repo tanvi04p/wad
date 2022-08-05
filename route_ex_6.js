2ff7dcdb3bd21c34f5748a403361df6894d81e12
tanvi04p <101508465+tanvi04p@users.noreply.github.com>
2022-08-05 14:48:16 +0530

    Add files via upload

---
+++ route_ex_6.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app= express();
+app.get('/filter_students_by/:dept/:batch',function(req, res){
+res.send("<h3>List of students filtered by</h3>"+
+"Department:"+req.params.dept +
+"<br> Batch:" +req.params.batch);
+});
+app.listen(8000);
+//URL: http://localhost:8000/msu/1234
